0d654a0275fb8e451cd0f834d73ac4cb05e00614
mrqs <146396924+pedrooomrqs@users.noreply.github.com>
2025-10-27 18:49:38 +0000

    Add files via upload

---
+++ Ficha 1/1 - helloWorld.py
@@ -0,0 +1,12 @@
+"""
+    @pedro.reis
+"""
+
+import math
+
+print("Ola, Mundo!!")
+print ("\nEste e o meu primeiro programa na linguagem de Python.")
+print("Apenas faz output de mensagens")
+print("\nProgramador: Pedro Reis")
+print("\nPrima ENTER para terminar...")
+input()
---
+++ Ficha 1/3 - mediaTres.py
@@ -0,0 +1,13 @@
+"""
+    @pedro.reis
+"""
+
+print("== MEDIA DE 3 NUMEROS ==\n")
+
+num1 = float(input("Escolha o 1. numero >> "))
+num2 = float(input("Escolha o 2. numero >> "))
+num3 = float(input("Escolha o 3. numero >> "))
+
+media = (num1 + num2 + num3) / 3
+
+print("\nMedia de", num1, "|", num2, "|", num3, "=", media)
