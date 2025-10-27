938c90b16837626899cf406e754e6b35c3de7311
mrqs <146396924+pedrooomrqs@users.noreply.github.com>
2025-10-27 20:35:59 +0000

    Delete Ficha 1 directory

--- Ficha 1/2 - antesDepois.py
+++
@@ -1,7 +0,0 @@
-"""
-    @pedro.reis
-"""
-
-print("== ANTECESSOR E SUCESSOR DE UM N. INTEIRO")
-num = int(input("\nEscolha um numero >> "))
-print("\nAntecessor :", num - 1, "; Sucessor :", num + 1)
